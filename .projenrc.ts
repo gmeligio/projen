0b38a566f38d1b1b3aa736c141c8989787af49d2
Eligio Mariño <22875166+gmeligio@users.noreply.github.com>
2024-03-07 22:54:53 +0100

    build: remove prepare task from .projen/tasks.json and .projenrc.ts

--- .projenrc.ts
+++ .projenrc.ts
@@ -396,21 +396,21 @@ function setupBundleTaskRunner() {
   project.postCompileTask.spawn(task);
 }
 
-function setupPrepareTask() {
-  const packageJsTask = project.tasks.tryFind("package:js");
+// function setupPrepareTask() {
+//   const packageJsTask = project.tasks.tryFind("package:js");
 
-  const prepareTask = project.addTask("prepare", {
-    description: "It will run before the package is packaged and installed",
-  });
+//   const prepareTask = project.addTask("prepare", {
+//     description: "It will run before the package is packaged and installed",
+//   });
 
-  if (packageJsTask) {
-    prepareTask.spawn(packageJsTask);
-  }
-}
+//   if (packageJsTask) {
+//     prepareTask.spawn(packageJsTask);
+//   }
+// }
 
 setupIntegTest();
 setupBundleTaskRunner();
-setupPrepareTask();
+// setupPrepareTask();
 
 // we are projen, so re-synth after compiling.
 // fixes feedback loop where projen contibutors run "build"
